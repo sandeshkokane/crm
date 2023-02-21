d9b8edd1229f1cec36547b0078548e5eb617eb1f
sandeshkokane <113164347+sandeshkokane@users.noreply.github.com>
2023-02-21 13:15:58 +0530

    Add files via upload

---
+++ test1.java
@@ -0,0 +1,14 @@
+package seleniumautomation;
+
+public class test1 
+{
+
+	public static void main(String[] args)
+	{
+		webdriver driver=new ChromeDriver();
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
